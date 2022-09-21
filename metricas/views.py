577b1d76c4c47ec259fec074e5c0c039a2c30058
Erick Nieto <erick.nieto@zabud.com.co>
2022-09-20 23:40:10 -0500

    init

---
+++ metricas/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render, redirect
+from metricas.forms import MetricsForm
+from metricas.models import Metrics
+# Create your views here.
+
+
+def diary(request):
+    if request.method == "POST":
+        form = MetricsForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('/show')
+            except:
+                pass
+    else:
+        form = MetricsForm()
+    return render(request, 'index.html', {'form': form})
+
+
+def show(request):
+    metric = Metrics.objects.all()
+    return render(request, "show.html", {'metric': metric})
+
+
+def edit(request, id):
+    metrics = Metrics.objects.get(id=id)
+    # stressLevel = Metrics.
+    return render(request, 'edit.html', {'metrics': metrics, })
+
+
+def update(request, id):
+    metrics = Metrics.objects.get(id=id)
+    form = MetricsForm(request.POST, instance=metrics)
+    #stressLevels = Metrics.stresslevel.choices
+    if form.is_valid():
+        form.save()
+        return redirect("/show")
+    return render(request, 'edit.html', {'metrics': metrics})
+
+
+def destroy(request, id):
+    metrics = Metrics.objects.get(id=id)
+    metrics.delete()
+    return redirect("/show")
